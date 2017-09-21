404da4927fa1151445991ed063a741abe3a8c191
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-21 02:13:08 -0700

    creating first react component

---
+++ app/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+// state
+// lifecycle event
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+
+);
+
+// 12:20
