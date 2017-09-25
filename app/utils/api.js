e450e25415094e59c4f54fe74035edb538bdc1af
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-25 15:40:31 -0700

    added utils folder, then api.js file within it for ajax requests

---
+++ app/utils/api.js
@@ -0,0 +1,7 @@
+var axios = require('axios');
+
+module.exports = {
+  fetchPopularRepos: function (language) {
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+  }
+}
