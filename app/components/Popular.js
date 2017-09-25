3505217b371e4b9be772d7f2e40c7dffd306112d
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-24 20:10:14 -0700

    selected languages is working appropriately, everything in accordance as expected

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,18 +1,37 @@
 var React = require('react');
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang) {
+    this.setState(function () {
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+
   render() {
     var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
 
     return (
       <ul className='languages'>
+        <p>Selected Language: {this.state.selectedLanguage}</p>
         {languages.map(function (lang) {
           return (
-            <li>
+            <li
+              onClick={this.updateLanguage.bind(null, lang)}
+              key={lang}>
               {lang}
             </li>
           )
-        })}
+        }, this)}
       </ul>
     )
   }
