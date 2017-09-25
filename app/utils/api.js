8d079dc0aa40d854d3afce097fe8e934ae1a442f
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-25 15:52:30 -0700

    api.js file bug fixes

--- app/utils/api.js
+++ app/utils/api.js
@@ -4,8 +4,8 @@ module.exports = {
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
-  return axios.get(encodeURI)
-    .then(function () {
+  return axios.get(encodedURI)
+    .then(function (response) {
       return response.data.items;
     });
   }
