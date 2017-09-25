8419d351ce776d7584f193d02786360f2a91485a
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-25 15:43:35 -0700

    api.js file completed

--- app/utils/api.js
+++ app/utils/api.js
@@ -3,5 +3,10 @@ var axios = require('axios');
 module.exports = {
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+
+  return axios.get(encodeURI)
+    .then(function () {
+      return response.data.items;
+    });
   }
 }
