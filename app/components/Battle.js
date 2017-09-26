a822ace4fa046e46dde6c09d0a1481311a826096
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-26 16:44:51 -0700

    updating battle.js file for usage of the full functionality of the battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,24 @@
 var React = require('react');
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName = '',
+      playerTwoName = '',
+      playerOneImage = null,
+      playerTwoImage = null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit() {
+    this.setState(function() {
+
+    })
+  }
+
   render() {
     return (
       <div>
