65b938b00346196192a17453c148ed18f3080528
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-25 23:24:54 -0700

    nav component complete

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,9 +1,25 @@
 var React = require('react');
+var NavLink = require('react-router-dom').NavLink;
 
 function Nav () {
   return (
     <ul className='nav'>
       <li>
+        <NavLink activeClassName='active' to='/'>
+          Home
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink activeClassName='active' to='/battle'>
+          Battle
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink activeClassName='active' to='/popular'>
+          Popular
+        </NavLink>
       </li>
     </ul>
   )
