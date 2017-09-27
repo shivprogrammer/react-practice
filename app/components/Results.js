e3c3cdff01c4c1ac2bc7f34ec8737f841edaae78
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-27 02:45:18 -0700

    lots of progress on the results.js component

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var queryString = require('query-string');
 var api = require('../utils/api');
+var Link = require('react-router-dom').Link;
 
 class Results extends React.Component {
   constructor(props) {
@@ -19,12 +20,57 @@ class Results extends React.Component {
       players.playerOneName,
       players.playerTwoName
     ]).then(function (results) {
-      console.log(results);
-    })
+      if (results === null) {
+        return this.setState(function () {
+          return {
+            error: 'Damn son, there is an error. Double check to make sure both the users are on github',
+            loading: false
+          }
+        })
+      }
+
+      this.setState(function () {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+      })
+    }.bind(this));
   }
   render() {
+    var error = this.state.error;
+    var winner = this.state.winner;
+    var loser = this.state.loser;
+    var loading = this.state.loading;
+
+    if (loading === true) {
+      return <p>Loading</p>
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to='/battle'>Reset</Link>
+        </div>
+      )
+    }
+
     return (
-      <div>Results</div>
+      <div className='row'>
+        <Player
+          label='Winner'
+          score={winner.score}
+          profile={winner.profile}
+        />
+        <Player
+          label='Loser'
+          score={loser.score}
+          profile={loser.profile}
+        />
+      </div>
     )
   }
 }
