afc97ce7cd1fa2ce26f1162238750dbdbc943b8c
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-26 17:31:13 -0700

    player input class completed

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,9 +2,57 @@ var React = require('react');
 var PropTypes = require('prop-types');
 
 class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      username: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    var value = event.target.value;
+
+    this.setState(function() {
+      return {
+        username: value
+      }
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    )
+  }
+
   render() {
     return (
-
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>
+          {this.props.label}
+        </label>
+        <input
+          id='username'
+          placeholder='github username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button
+          className='button'
+          type='submit'
+          disabled={!this.state.username}>
+            Submit
+          </button>
+      </form>
     )
   }
 }
@@ -20,10 +68,10 @@ class Battle extends React.Component {
     super(props);
 
     this.state = {
-      playerOneName = '',
-      playerTwoName = '',
-      playerOneImage = null,
-      playerTwoImage = null
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
