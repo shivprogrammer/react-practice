4579a678224a61c7fd4487f13c4c969b89236a87
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-27 02:31:20 -0700

    working on results.js page for the results of the github battle:

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -169,7 +169,7 @@ class Battle extends React.Component {
             className='button'
             to={{
               pathname: match.url + '/results',
-              search: '?playerOneName=' + playerOneName + '?playerTwoName=' + playerTwoName
+              search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
             }}>
               Battle
           </Link>}
