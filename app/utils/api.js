35178bc0717716d2db0643f6de064b5ad8a75919
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-26 20:36:50 -0700

    adding ajax requests in api.js using axios

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,31 @@
 var axios = require('axios');
 
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var params = "?client_id=" + id + "&client_secret=" + sec;
+
+function getProfile (username) {
+  return axios.get('https://api.github.com/users/' + username + params)
+    .then(function (user) {
+      return user.data;
+    })
+}
+
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+}
+
+function getStarCount (repos) {
+  return repos.data.reduce(function (count, repo) {
+    return count + repo.stargazers_count;
+  }, 0)
+}
+
 module.exports = {
-  fetchPopularRepos: function (language) {
+  battle: function (players) {
+
+  },
+   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
   return axios.get(encodedURI)
