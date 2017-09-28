52b9f22142fbae9be7c6bcf5459bfaf85a9fd1df
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-27 22:21:25 -0700

    started Loading component

---
+++ app/components/Loading.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+class Loading extends React.Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+module.exports = Loading;
