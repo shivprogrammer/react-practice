dda3aa57fa752ae5c847c3feb22801e713c66007
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-27 22:30:49 -0700

    progress on Loading.js

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,10 +1,26 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+var styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+}
+
 class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: 'Loading'
+    }
+  }
   render() {
     return (
-
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
     )
   }
 }
